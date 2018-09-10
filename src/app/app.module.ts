a88953dc287c63c965f54d2eb5890ab1344193f4
MakhongoananaM <39265018+MakhongoananaM@users.noreply.github.com>
2018-09-10 09:38:54 +0200

    First changes for the day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    LoginPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    LoginPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
-})
-export class AppModule {}
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -68,4 +33,3 @@ import { ChefsFridgeProvider } from '../providers/chefs-fridge/chefs-fridge';
   ]
 })
 export class AppModule {}
->>>>>>> 027cef3416bd63e31a14e1d68cb78b5d343b9b0a
